7cb40f4e6d2a1372664872b856b6b961a5a70047
Onion <rrhs0495@gmail.com>
2024-10-21 16:53:30 +0500

    small change

--- src/components/pageTable.tsx
+++ src/components/pageTable.tsx
@@ -244,7 +244,7 @@ export default function Tables() {
                                     onClick={() =>
                                         backend
                                             .deleteProject(user.id)
-                                            .then(() => router.refresh())
+                                            .then(() => router.push("/projects"))
                                     }
                                 >
                                     Удалить
